d3dcb6eefb8a896e6888da9348690653fdff5ac8
ceccs17d18 <43025271+ceccs17d18@users.noreply.github.com>
2018-11-27 12:16:13 -0500

    Add files via upload

---
+++ 15.c
@@ -0,0 +1,103 @@
+//15.representation of polynomial using ll//
+#include <stdio.h>//darsana.c,roll no:18//
+#include <stdlib.h>
+#include <string.h>
+
+struct node
+{
+    int cof;
+    int exp;
+    struct node *link;
+};
+
+struct node *create(struct node *q);
+struct node *insert(struct node *ptr, struct node *p);
+void display(char const *tag, struct node *ptr);
+void err_exit(char const *tag);
+
+struct node *create(struct node *q)
+{
+    int i, n;
+    printf("enter the number of nodes: ");
+    if (scanf("%d", &n) != 1)
+        err_exit("Read error (number of nodes)");
+    for (i = 0; i < n; i++)
+    {
+        struct node *ptr = (struct node *)malloc(sizeof(struct node));
+        if (ptr == 0)
+            err_exit("Out of memory (1)");
+        printf("enter the coefficient and exponent respectively: ");
+        if (scanf("%d%d", &ptr->cof, &ptr->exp) != 2)
+            err_exit("Read error (coefficient and exponent)");
+        ptr->link = NULL;
+        q = insert(ptr, q);
+        display("after input", q);
+    }
+    return q;
+}
+
+struct node *insert(struct node *ptr, struct node *p)
+{
+    struct node *temp, *b;
+    if (p == NULL)
+        p = ptr;
+    else
+    {
+        display("insert: p   = ", p);
+        display("insert: ptr = ", ptr);
+        if (p->exp < ptr->exp)
+        {
+            ptr->link = p;
+            p = ptr;
+        }
+        else
+        {
+            temp = p;
+            while ((temp->link != NULL) && (temp->link->exp < ptr->exp))
+                display("insert: tmp = ", temp),
+                temp = temp->link;
+            display("insert: post loop", temp);
+            b = temp->link;
+            temp->link = ptr;
+            ptr->link = b;
+        }
+    }
+    return p;
+}
+
+void display(char const *tag, struct node *ptr)
+{
+    struct node *temp;
+    const char *pad = "";
+    temp = ptr;
+    printf("%s: ", tag);
+    while (temp != NULL)
+    {
+        printf("%s%d x ^ %d", pad, temp->cof, temp->exp);
+        temp = temp->link;
+        pad = " + ";
+    }
+    putchar('\n');
+}
+
+int main(void)
+{
+    printf("enter the first polynomial:\n");
+    struct node *p1 = NULL, *p2 = NULL;
+
+    p1 = create(p1);
+
+    printf("enter the second polynomial:\n");
+    p2 = create(p2);
+
+    display("p1", p1);
+    display("p2", p2);
+
+    return 0;
+}
+
+void err_exit(char const *tag)
+{
+    fprintf(stderr, "%s\n", tag);
+    exit(1);
+}
